7e97f9db7f44c9b5e425f0372e44ecf6ce0205b5
Prasad Arbad <prasadarbad9@gmail.com>
2022-03-08 09:04:03 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/screens/products_overview_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ProductsOverViewScren(),
+    );
+  }
+}
