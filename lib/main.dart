5005e4ced3014bf0b6ac73719aa6bd004e532550
Prasad Arbad <prasadarbad9@gmail.com>
2022-03-08 11:19:54 +0530

    fisrt commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MyShop',
+      title: 'Shop App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
