d338d1f2998c584cc20359381c211080a1fac754
Prasad Arbad <prasadarbad9@gmail.com>
2022-03-08 11:24:40 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Shop App',
+      title: 'Shop Apps',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
